63e76cfe0a1fd3d00f84527c2aaecae2efdb78b8
Andreas Woerle <andi@brueckenschlager.de>
2012-01-18 22:54:58 +0100

    fixing heavy bug

--- lib/call.rb
+++ lib/call.rb
@@ -3,91 +3,6 @@
 ## proudly presented by adap:to
 ## devolute.org
 
-require 'net/http'
-
-USHAHIDI_CONFIG = {
-  :url => 'ec2-50-112-5-172.us-west-2.compute.amazonaws.com/admin',
-  :parameters => {
-    :required => { },
-    :optional => {
-      :task => 'task',
-      :by => 'by',
-      :action => 'action',
-      :incident_id => 'incident_id'
-    },
-  },
-  :authentication => {
-    :basic_auth => {
-      :user => 'admin',
-      :password => 'admin' },
-  },
-}
-
-class UshahidiGateway
-
-  attr_reader :user, :password
-
-  def initialize(credentials)
-    @user = credentials[:user]
-    @password = credentials[:password]
-  end
-
-  def get url
-     raise "implement get method for the Ushahidi gateway"
-  end
-
-  def post url, payload
-    response = Net::HTTP.post_form(URI.parse("http://#{user}:#{password}@" + url), payload )
-    response.body
-  end
-
-end
-
-class UshahidiClient
-
-  attr_reader :config, :gateway, :url
-
-  def initialize
-    @config = USHAHIDI_CONFIG
-    @gateway = UshahidiGateway.new( config[:authentication][:basic_auth] )
-    @url = config[:url]
-  end
-
-
-  def post_report report
-    gateway.post( url, build_options_from(report) )
-  end
-
-  private
-
-  def build_options_from report
-    payload = {
-      :task => 'report',
-      :incident_title => '',
-      :incident_description => report[:incident_description],
-      :incident_category => report[:category],
-      :incident_date => get_date_and_time[:date],
-      :incident_hour => get_date_and_time[:hours],
-      :incident_minute => get_date_and_time[:minutes],
-      :incident_ampm => get_date_and_time[:am_pm],
-      :latitude => report[:latitude],
-      :longitude => report[:longitude],
-      :location_name => ''
-    }
-  end
-
-  def get_date_and_time
-    t = Time.new
-    time_hash = { }
-    time_hash[:am_pm]= t.strftime("%P")
-    time_hash[:hours]= t.strftime("%I")
-    time_hash[:minutes]= t.strftime("%M")
-    time_hash[:date]= t.strftime("%m/%d/%Y")
-    time_hash
-  end
-
-end
-
 
 module LocalTesting
 
@@ -292,21 +207,21 @@ class Call
     # store basic caller information (name, number, set retries to 0)
     store_initial_caller_info if $currentCall.isActive
 
-    say(isay "0_1_Welcome_Message") if $currentCall.isActive
+    say(isay("0_1_Welcome_Message")) if $currentCall.isActive
 
-    wait(100) unless if $currentCall.isActive
+    wait(100) if $currentCall.isActive
 
     # retries getting the site till successful, or kicks out the user after too many retries
     # after it ran successfully we have a @site instance variable with the chosen site
-    get_site_info unless if $currentCall.isActive
+    get_site_info if $currentCall.isActive
 
     # gets current incident code
     # stores the incident action, or kicks out after several retries
-    get_incident_code_and_type! unless if $currentCall.isActive
+    get_incident_code_and_type! if $currentCall.isActive
 
     # either asks for the amount of money
     # or in emergency sends report to call the number
-    incident_action! unless if $currentCall.isActive
+    incident_action! if $currentCall.isActive
 
     # report = build_report caller_info
   end
@@ -531,8 +446,6 @@ class Call
     hangup!
   end
 
-
 end
-
 
 Call.new.run
